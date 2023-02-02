1ab65a097e65e713891cadb65276d6c76bd59211
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-02-01 17:45:47 -0800

    add(): config module import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { CountryModule } from './country/country.module';
 
 
 @Module({
-    imports: [CountryModule]
+    imports: [
+        ConfigModule.forRoot(),
+
+        CountryModule
+    ]
 })
 export class AppModule {}
