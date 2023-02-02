2cd79c01eb04de00666aa63b843b569371d48a29
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-02-01 18:29:04 -0800

    add(): country-dto structure

--- src/country/dto/create-country.dto.ts
+++ src/country/dto/create-country.dto.ts
@@ -1,1 +1,19 @@
-export class CreateCountryDTO {}
+import { Transform } from "class-transformer";
+import { IsISO31661Alpha2, IsISO31661Alpha3, IsOptional, IsString } from "class-validator";
+
+
+export class CreateCountryDTO
+{
+    @IsString()
+    country_name:     string;
+
+    @IsISO31661Alpha2()
+    @IsOptional()
+    @Transform( ({ value }) => (value.toUpperCase()) )
+    two_alpha_abbr:   string;
+
+    @IsISO31661Alpha3()
+    @IsOptional()
+    @Transform( ({ value }) => (value.toUpperCase()) )
+    three_alpha_abbr: string;
+}
