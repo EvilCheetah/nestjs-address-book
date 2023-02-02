73307bf14c65a6c2a3a5ea42c5cb4f577f7a1261
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-02-01 18:14:49 -0800

    add(): basic structure for country

--- src/country/entities/country.entity.ts
+++ src/country/entities/country.entity.ts
@@ -1,1 +1,18 @@
-export class Country {}
+import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+
+
+@Entity()
+export class Country
+{
+    @PrimaryKey()
+    country_id:       number;
+
+    @Property({ unique: true })
+    country_name:     string;
+
+    @Property({ unique: true, nullable: true, length: 2 })
+    two_alpha_abbr:   string;
+
+    @Property({ unique: true, nullable: true, length: 3 })
+    three_alpha_abbr: string;
+}
