c5a6f4ce8eeecc6a9325540b047cf72ad3cb545c
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-02-01 21:34:36 -0800

    add(): basic crud

--- src/country/country.service.ts
+++ src/country/country.service.ts
@@ -1,33 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { EntityRepository, wrap } from '@mikro-orm/core';
+import { InjectRepository } from '@mikro-orm/nestjs';
+
+import { Country } from './entities/country.entity';
 import { CreateCountryDTO } from './dto/create-country.dto';
 import { UpdateCountryDTO } from './dto/update-country.dto';
+import { CountryNotFoundException } from './errors/country-not-found.exception';
 
 
 @Injectable()
 export class CountryService
 {
-    create(createCountryDTO: CreateCountryDTO)
+    constructor(
+        @InjectRepository(Country)
+        private readonly countryRepository: EntityRepository<Country>
+    ) {}
+
+    async create(createCountryDTO: CreateCountryDTO)
     {
-        return 'This action adds a new country';
+        const country = this.countryRepository.create(createCountryDTO);
+
+        await this.countryRepository.persistAndFlush(country);
+
+        return country;
     }
 
-    findAll()
+    find_all()
     {
-        return `This action returns all country`;
+        return this.countryRepository.findAll();
     }
 
-    findOne(country_id: number)
+    async find_one(country_id: number)
     {
-        return `This action returns a #${country_id} country`;
+        const country = await this.countryRepository.findOne({ country_id });
+
+        if ( !country )
+            throw new CountryNotFoundException(country_id);
+
+        return country
     }
 
-    update(country_id: number, updateCountryDTO: UpdateCountryDTO)
+    async update(country_id: number, updateCountryDTO: UpdateCountryDTO)
     {
-        return `This action updates a #${country_id} country`;
+        const country = await this.find_one(country_id);
+
+        wrap(country).assign(updateCountryDTO);
+        await this.countryRepository.persistAndFlush(country);
+
+        return country;
     }
 
-    remove(country_id: number)
+    async remove(country_id: number)
     {
-        return `This action removes a #${country_id} country`;
+        const country = await this.find_one(country_id);
+
+        await this.countryRepository.removeAndFlush(country);
+
+        return country;
     }
 }
