cad90c0975b65388b51d05e184a44783054a7db8
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-02-01 21:52:48 -0800

    add(): mikro orm import

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { validationSchema } from '@/validation';
+import { get_database_config } from '@/config';
 import { CountryModule } from './country/country.module';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 
 
 @Module({
     imports: [
         ConfigModule.forRoot({ validationSchema }),
+        MikroOrmModule.forRootAsync( get_database_config() ),
 
         CountryModule
     ]
