983a3c155cd9bb13498d94eccb44142559d14cff
Eugene Moshchyn <eugene.moshchyn@gmail.com>
2023-02-01 18:33:52 -0800

    add(): register repository for country

--- src/country/country.module.ts
+++ src/country/country.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+
+import { Country } from './entities/country.entity';
 import { CountryService } from './country.service';
 import { CountryController } from './country.controller';
 
 
 @Module({
+    imports:     [MikroOrmModule.forFeature([ Country ])],
     controllers: [CountryController],
     providers:   [CountryService]
 })
